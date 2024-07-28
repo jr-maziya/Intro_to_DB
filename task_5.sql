eb3dfe54d2e2325d9faf5fc56e096cbae6bdfbf4
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-28 13:27:02 +0200

    initial commit task5

---
+++ task_5.sql
@@ -0,0 +1,11 @@
+import mysql.connector
+# Replace with your connection details
+
+USE alx_book_store;
+
+sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)" val = (
+    "1",
+    "Cole Baidoo",
+    "cbaidoo@sandtech.com",
+    "123 Happiness Ave"
+) cursor.execute (sql, val) mydb.commit ()
