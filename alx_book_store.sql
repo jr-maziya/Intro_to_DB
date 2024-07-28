40302d317291483a3e7a36dca9fce5c61fa1ee5e
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-28 10:52:01 +0200

    Prettier Extension making the checker fail

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,9 +31,9 @@ CREATE TABLE Orders (
 )
 
 CREATE TABLE Order_Details (
-    orderdetailid PRIMARY KEY,
-    order_id,
-    book_id,
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders (order_id),
     FOREIGN KEY (book_id) REFERENCES Books (book_id)
