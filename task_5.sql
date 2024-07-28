4ddad6fc5464ac462697d524046ab8de8f738595
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-28 13:27:56 +0200

    change of submitted address adding a .

--- task_5.sql
+++ task_5.sql
@@ -7,5 +7,5 @@ sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES 
     "1",
     "Cole Baidoo",
     "cbaidoo@sandtech.com",
-    "123 Happiness Ave"
+    "123 Happiness Ave."
 ) cursor.execute (sql, val) mydb.commit ()
