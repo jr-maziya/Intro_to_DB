df8c065b4563255de4cbb56471f97e6b93c246e8
Geoff Maziya <geoffmaziya04@gmail.com>
2024-07-28 12:26:11 +0200

    initial commit

---
+++ MySQLServer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+try:
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Backend@ALX2024"
+    )
+
+    print("Connection to database successful")
+
+except:
+    print("Connection to database failed")
+
+cursor = mydb.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+print("Database 'alx_book_store' created successfully!")
+
